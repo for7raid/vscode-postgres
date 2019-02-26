03188b6b12d8490920bf88202437e319c56f2622
Alexey Karov <akarov@pmpractice.ru>
2019-02-26 16:56:52 +0300

    форматер

--- src/formatter/PsqlDocumentFormattingEditProvider.ts
+++ src/formatter/PsqlDocumentFormattingEditProvider.ts
@@ -1,10 +1,10 @@
 import * as vscode from "vscode";
-import * as sqlFormatter from 'sqlFormatter.js';
+import * as sqlFormatter from 'sql-formatter';
 
 export default class PsqlDocumentFormattingEditProvider implements vscode.DocumentFormattingEditProvider {
 	provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
 		let text = document.getText();
-		text = sqlFormatter.format(text, { language: "pl/sql" });
+		text = sqlFormatter.format(text, { language: "pl/sql", indent: '\t' });
 		return [vscode.TextEdit.replace(new vscode.Range(0, 0, 9999, 9999), text)];
 	}
 }
